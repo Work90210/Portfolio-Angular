96876eba8183ff794593a0e7d292c843d664c8b8
Work90210 <kyle.fuhri@yahoo.com>
2020-11-26 11:20:54 +0200

    navbar added & new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { WebDevelopmentComponent } from './web-development/web-development.component';
+import { WordpressComponent } from './wordpress/wordpress.component';
+import { FlutterComponent } from './flutter/flutter.component';
+import { DesignComponent } from './design/design.component';
+import { OverallComponent } from './overall/overall.component';
+import { HobbiesComponent } from './hobbies/hobbies.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    WebDevelopmentComponent,
+    WordpressComponent,
+    FlutterComponent,
+    DesignComponent,
+    OverallComponent,
+    HobbiesComponent,
+    ExperienceComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,

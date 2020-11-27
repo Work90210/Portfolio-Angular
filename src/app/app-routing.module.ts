9e9a0ba09875380b1e1bff71edf214848647a7f4
Work90210 <kyle.fuhri@yahoo.com>
2020-11-27 11:26:29 +0200

    Home page updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { DesignComponent } from './design/design.component';
+import { FlutterComponent } from './flutter/flutter.component';
+import { HobbiesComponent } from './hobbies/hobbies.component';
+import { HomeComponent } from './home/home.component';
+import { OverallComponent } from './overall/overall.component';
+import { WebDevelopmentComponent } from './web-development/web-development.component';
+import { WordpressComponent } from './wordpress/wordpress.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'development', component: WebDevelopmentComponent},
+  {path: 'wordpress', component: WordpressComponent},
+  {path: 'flutter', component: FlutterComponent},
+  {path: 'design', component: DesignComponent},
+  {path: 'overall', component: OverallComponent},
+  {path: ' hobbies', component: HobbiesComponent},
+  {path: 'contact', component: ContactComponent},
+  // Portfolio pieces to be added below...
+  // {path: 'portfolio', component: PortfolioComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { WebDevelopmentComponent } from './web-development/web-development.component';
+import { WordpressComponent } from './wordpress/wordpress.component';
+import { FlutterComponent } from './flutter/flutter.component';
+import { DesignComponent } from './design/design.component';
+import { OverallComponent } from './overall/overall.component';
+import { HobbiesComponent } from './hobbies/hobbies.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    WebDevelopmentComponent,
+    WordpressComponent,
+    FlutterComponent,
+    DesignComponent,
+    OverallComponent,
+    HobbiesComponent,
+    ExperienceComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]

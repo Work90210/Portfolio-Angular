4154a6f7c1d96d9949cd68f6316c33b52537fef1
Work90210 <kyle.fuhri@yahoo.com>
2020-11-30 09:02:46 +0200

    user model created

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user-model';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
+  user: User = {
+    profilePic: "https://mdbootstrap.com/img/Photos/Avatars/img%20(8).jpg",
+    name: "Kyle Fuhri",
+    title: "Intern Junior Web Developer",
+    workName: "Liber8",
+    workWebsite: "https://www.liber8.co.za",
+    city: "Johannesburg",
+    country: "South Africa",
+    facebookLink: "https://www.facebook.com/kyle.fuhri",
+    githubLink: "https://github.com/Work90210",
+    linkedinLink: "https://linkedin.com/in/kyle-fuhri",
+    description: "This is where people would add a little description about themselves...",
+  };
 
   constructor() { }
 
---
+++ src/app/models/user-model.ts
@@ -0,0 +1,14 @@
+export class User {
+  // Error will occur when not initialized
+  profilePic!: string;
+  name!: string;
+  title!: string;
+  workName!: string;
+  workWebsite!: string;
+  city!: string;
+  country!: string;
+  facebookLink!: string;
+  githubLink!: string;
+  linkedinLink!: string;
+  description!: string;
+}
